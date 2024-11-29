9d85ecb8949e159886deeee9978a4b76ced47533
Thilina Lakshitha <108370639+lakshitha213@users.noreply.github.com>
2024-11-29 09:56:16 +0000

    create a AboutUs page

---
+++ myapp/src/Component/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutUs=()=>{
+    return(
+        <>
+        <div>
+            <h1>Welcome to About page</h1>
+        </div>
+        </>
+    );
+};
+
+export default AboutUs;
